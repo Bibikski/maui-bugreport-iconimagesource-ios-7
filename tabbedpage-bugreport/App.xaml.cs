77fe5b97c082bdf240b43e4944b4e07ded5189a4
Bibikski <bibikski@gmail.com>
2023-04-29 21:25:52 -0400

    BugReport Check-in

---
+++ tabbedpage-bugreport/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace tabbedpage_bugreport;
+
+public partial class App : Application
+{
+	public App(NavigationManager _manager)
+	{
+		InitializeComponent();
+		MainPage = new NavigationPage(new MainPage());
+	}
+}
---
+++ tabbedpage-bugreport/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace tabbedpage_bugreport;
+
+public partial class MainPage : TabbedPage 
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
